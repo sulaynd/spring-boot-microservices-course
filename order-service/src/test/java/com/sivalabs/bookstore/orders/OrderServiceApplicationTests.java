41ca2324354abea89c2f7ccbf065a9d1ddbbfa2c
souleye <sulaynd@gmail.com>
2024-08-28 12:11:11 -0400

    modify tests files

--- order-service/src/test/java/com/sivalabs/bookstore/orders/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/sivalabs/bookstore/orders/OrderServiceApplicationTests.java
@@ -1,7 +1,9 @@
 package com.sivalabs.bookstore.orders;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(classes = OrderServiceApplicationTests.class)
 class OrderServiceApplicationTests extends AbstractIT {
 
     @Test
